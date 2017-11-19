1427ba304715ccdd03eeb037bbeb35883a6391f7
Arno Schutijzer <arno.s@outlook.com>
2017-11-19 14:48:40 +0100

    chore(deps): update npm dependencies so router state is fixed

--- tools/generator/component/temp.module.js
+++ tools/generator/component/temp.module.js
@@ -1,5 +1,5 @@
 import angular from 'angular';
-import uiRouter from 'angular-ui-router';
+import uiRouter from '@uirouter/angularjs';
 import <%= namePascal %>Component from './<%= nameKebab %>.component';
 
 /**
